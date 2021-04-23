7d4e9030bd9688867814e4fb4df6b7f5c3d7cc47
abhinav-yadav-xooa <abhinavtan90@gmail.com>
2021-04-24 00:55:33 +0530

    cahnges in function name of post request

--- main.py
+++ main.py
@@ -14,17 +14,16 @@ def home():
     return HTMLResponse(content=html, status_code=200)
 
 
-@app.post("/post",response_class=HTMLResponse)
-async def image(sentdata: UploadFile = File(...)):
+@app.post("/post", response_class=HTMLResponse)
+async def upload1(sentdata: UploadFile = File(...)):
     with open(f"{sentdata.filename}", "wb") as buffer:
         shutil.copyfileobj(sentdata.file, buffer)
     return {"filename": sentdata.filename}
 
 
 @app.post("/post1")
-async def image(request: Request):
+async def upload(request: Request):
     da = await request.body()
     print(da)
     da1 = jsonable_encoder(da)
     return da1
-
